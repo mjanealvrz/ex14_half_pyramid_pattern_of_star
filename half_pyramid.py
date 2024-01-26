976a6c44e15c1a222e4bad4ed2f41fe2fce55b93
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 15:21:21 +0800

    Created for loop and printed the result

--- half_pyramid.py
+++ half_pyramid.py
@@ -5,6 +5,10 @@
 # Set rows to 5
 rows = 5
 # Create for loop
+for i in range(rows, 0, -1):
      # Outer loop to iterate through each row from top to bottom
+    for j in range(1, i + 1):
     # Inner loop to print stars in each row
+        print("*", end= " ")
     # Move to the next line after printing stars in the current row
+    print()
