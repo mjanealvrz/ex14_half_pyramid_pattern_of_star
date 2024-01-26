6c5840b1def97cdcd6ca899fb7b070dcd053f48d
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 15:17:19 +0800

    Added program statement and Pseudocode

---
+++ half_pyramid.py
@@ -0,0 +1,9 @@
+# Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+# Pseudocode
+
+# Set rows to 5
+# Create for loop
+     # Outer loop to iterate through each row from top to bottom
+    # Inner loop to print stars in each row
+    # Move to the next line after printing stars in the current row
